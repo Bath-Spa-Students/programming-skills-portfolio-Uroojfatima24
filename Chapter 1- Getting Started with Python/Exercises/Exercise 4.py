0a19f22f5abcb494468c16260019360918df01e7
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2023-10-04 13:41:31 +0400

    Exercise 4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 4.py
@@ -0,0 +1,9 @@
+# Define three strings in different variables
+string1 = "See "
+string2 = "you "
+string3 = "later!"
+
+# Concatenate the strings into one
+output = string1 + string2 + string3
+
+print(output)
