6020d8a1c36b63bc43fc0a6d53aaa15cf68c56f2
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2023-10-17 09:17:29 +0400

    Exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 2.py
@@ -0,0 +1,6 @@
+
+author = "Oprah Winfrey"
+quote = "You can have it all. You just can't have it all at once."
+
+#Use f-string to support placeholders for variables.
+print(f'{author} once said, \"{quote}\"')
