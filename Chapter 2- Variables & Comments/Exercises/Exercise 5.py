c45beb723c170cc54c260b3fed4e0ccd6cf69397
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2023-10-17 09:18:28 +0400

    Exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 5.py
@@ -0,0 +1,8 @@
+
+USB_sticks_she_can_buy = 50//6 
+#// gives integer values
+
+pounds_left = 50 % 6 
+
+#arithmetic operators used: // , %
+print(f"She can buy {USB_sticks_she_can_buy} USB sticks and will have £{pounds_left} left.")
