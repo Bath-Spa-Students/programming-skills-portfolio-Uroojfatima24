f71b8c6ce136688f0996d2fa720040a3042a652d
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2023-10-22 22:27:28 +0400

    Exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -0,0 +1,6 @@
+# List of transportation examples
+Transportation = ["Motorcycle", "Car" , "Scooter", "Truck"]
+
+# Print statements about each item in the list
+for item in Transportation:
+    print(f"I would like to travel in a {item}.")
