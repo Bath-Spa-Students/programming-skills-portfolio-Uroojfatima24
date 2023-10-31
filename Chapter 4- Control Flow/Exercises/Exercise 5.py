2b13f03ae736eb23ffb356b6b5f45628df4bac83
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2023-10-31 10:19:41 +0400

    Exercise 5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 5.py
@@ -0,0 +1,16 @@
+favorite_fruits = ["oranges" , "mangoes" , "pomegranates"]
+
+if "oranges" in favorite_fruits:
+    print("You really like oranges!")
+
+if "mangoes" in favorite_fruits:
+    print("You really like mangoes!")
+
+if "pomegranates" in favorite_fruits:
+    print("You really like pomegranates!")
+
+if "apples" in favorite_fruits:
+    print("You really like apples!")
+
+if "apricots" in favorite_fruits:
+    print("You really like apricots!")
