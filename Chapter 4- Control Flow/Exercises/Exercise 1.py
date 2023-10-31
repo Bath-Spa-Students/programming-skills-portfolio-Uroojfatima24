1d3375cec8e06952daa93f1852f39494ad2e1834
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2023-10-31 10:18:03 +0400

    Exercise 1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 1.py
@@ -0,0 +1,12 @@
+alien_color = 'green'
+# Equal operator -> ==
+if alien_color == 'green':
+    print("You just earned 5 points!")
+#passes the if test
+
+
+alien_color = 'red'
+if alien_color == 'green':
+    print("You just earned 5 points!")
+#no output, fails the if test 
+
