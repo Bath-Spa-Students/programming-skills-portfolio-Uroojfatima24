6aeaf188f9ffbf795474c258e85e4d93f65a9f55
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2023-10-31 10:18:30 +0400

    Exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 2.py
@@ -0,0 +1,15 @@
+#This version runs the if block
+alien_color = 'green'
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the alien.")
+else:
+    print("You just earned 10 points.")
+
+
+#This version runs the else block
+alien_color = 'white'
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the alien.")
+else:
+    print("You just earned 10 points for shooting the alien.")
+
