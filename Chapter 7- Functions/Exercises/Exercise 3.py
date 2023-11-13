3e966c7668f83c4c95579fb88af4d3cc518a47fc
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2023-11-13 10:35:27 +0400

    Exercise 3.py

---
+++ Chapter 7- Functions/Exercises/Exercise 3.py
@@ -0,0 +1,11 @@
+def make_shirt(size, message):
+    #Prints a message about the size and message printed on a shirt.
+    print(f"A {size} shirt will be printed with the message: {message}")
+
+# Call the function using positional arguments
+make_shirt("Large", "One Piece")
+
+# Call the function using keyword arguments
+make_shirt(size="Medium", message="Attack on Titan")
+
+
