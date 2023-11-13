230ebd4cf8cee96a80cba0946295b5ac6a49d97e
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2023-11-13 12:52:33 +0400

    Practice 3.py

---
+++ Chapter 3- Data Structures/Exercises/Practice 3.py
@@ -0,0 +1,7 @@
+'''Assume names references a list. Write a for loop that displays each element of the list.'''
+
+names = ['Henry', 'Edward', 'Charles', 'Thomas']
+
+# Loop through each element in the list and display it
+for name in names:
+    print(name)
