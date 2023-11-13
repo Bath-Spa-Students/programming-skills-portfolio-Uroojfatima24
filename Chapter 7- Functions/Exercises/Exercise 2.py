3eb3b79a15e0b7339648f205b6d094597a99ad5d
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2023-11-13 10:35:04 +0400

    Exercise 2.py

---
+++ Chapter 7- Functions/Exercises/Exercise 2.py
@@ -0,0 +1,8 @@
+def favorite_book(title):
+#Prints a message about a favorite book.
+    message = f"One of my favorite books is {title}."
+    print(message)
+
+# Call the function with a book title as an argument
+favorite_book("Atomic Habits")
+
