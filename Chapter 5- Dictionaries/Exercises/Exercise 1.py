6824410e4d321749fcc1237b0ff009440000c671
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2023-11-13 10:31:21 +0400

    Exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 1.py
@@ -0,0 +1,18 @@
+Person_details = {
+    'first_name': 'Neji' ,
+    'last_name': 'Hyuga' ,
+    'age': '18' ,
+    'city': 'Yokohama'
+}
+
+'''A dictionary consists of key-value pairs, and you can access the values by specifying 
+the key associated with them.'''
+print("First Name: " + Person_details['first_name'])
+print("Last Name: " + Person_details['last_name'])
+print("Age: " + Person_details['age'])
+print("City: " + Person_details['city'])
+
+
+
+
+
