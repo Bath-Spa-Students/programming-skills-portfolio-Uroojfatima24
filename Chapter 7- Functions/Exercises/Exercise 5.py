346716f98530cdb9bed30b08b3ac3752e86c7878
Uroojfatima24 <144207316+Uroojfatima24@users.noreply.github.com>
2023-11-15 09:52:54 +0400

    Exercise 5.py

---
+++ Chapter 7- Functions/Exercises/Exercise 5.py
@@ -0,0 +1,8 @@
+def describe_city(city, country="Japan"):
+    #Prints a sentence describing the city and its country.
+    print(f"{city} is in {country}.")
+
+# Call the function for three different cities
+describe_city("Osaka")
+describe_city("Yokohama")
+describe_city("Tokyo")
